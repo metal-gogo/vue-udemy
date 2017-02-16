84766526da360c470d1f159e45595662e4852dbe
Daniel González <d.g.g69@me.com>
2017-02-16 10:49:16 -0600

    Finished Section 5, Class 75

--- Section-Code-Start/app.js
+++ Section-Code-Start/app.js
@@ -1,55 +1,36 @@
-Vue.component('hello', {
-  template: '<h1>Hello!!</h1>'
-});
-
-var vm1 = new Vue({
-  el: '#app1',
+new Vue({
+  el: '#app',
   data: {
+    counter: 0,
     title: 'The VueJS Instance',
-    showParagraph: false
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate()');
+  },
+  created: function() {
+    console.log('created()');
+  },
+  beforeMount: function() {
+    console.log('beforeMount()');
+  },
+  mounted: function() {
+    console.log('mounted()');
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate()');
+  },
+  updated: function() {
+    console.log('updated()');
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy()');
+  },
+  destroyed: function() {
+    console.log('destroyed()');
   },
   methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
-      this.$refs.myButton.innerText = 'TEST';
-    },
-    updateTitle: function(title) {
-      this.title = title;
-    }
-  },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
+    destroy: function() {
+      this.$destroy();
     }
   }
 });
-
-setTimeout(function(){
-  vm1.title = 'Changed by timer';
-  vm1.show();
-}, 2000);
-
-var vm2 = new Vue({
-  el: '#app2',
-  data: {
-    title: 'The Second VueJS Instance '
-  },
-  methods: {
-    onChange: function(){
-      vm1.title = 'Changed!';
-    }
-  }
-});
-
-var vm3 = new Vue({
-  template: '<h1>HELLO!!</h1>'
-});
-
-vm3.$mount();
-document.getElementById('app3').appendChild(vm3.$el);
