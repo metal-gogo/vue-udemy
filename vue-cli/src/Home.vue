1bba961c5f1ac4ac5ae4a32fd1baef0fab9e1895
Daniel González <d.g.g69@me.com>
2017-02-16 13:40:32 -0600

    Finished Section 7, Class 96

--- vue-cli/src/Home.vue
+++ vue-cli/src/Home.vue
@@ -1,22 +1,14 @@
 <template>
     <div>
-        <h2>Server Status: {{ status }}</h2>
-        <hr>
-        <button @click="changeStatus">Change Status</button>
+        <app-server-status v-for="server in 5"></app-server-status>
     </div>
 </template>
 
 <script>
+    import ServerStatus from './ServerStatus.vue';
     export default {
-        data: function() {
-            return {
-                status: 'Critical'
-            };
-        },
-        methods: {
-            changeStatus() {
-                this.status = 'Normal';
-            }
+        components: {
+            'app-server-status': ServerStatus
         }
     }
 </script>
