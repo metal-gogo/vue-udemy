4d2cebfe842a11888e8c9ccd457d418eb92395fe
Daniel González <d.g.g69@me.com>
2017-02-17 12:04:57 -0600

    Finished Section 9, Class 122

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,25 +2,30 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-user></app-user>
+               <app-quote>
+                   <h2 slot="title">{{ quoteTitle }}</h2>
+                   <p slot="content">{{ quoteContent }}</p>
+               </app-quote>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
+    import Quote from './components/Quote.vue';
 
     export default {
+        data() {
+            return {
+                quoteTitle: 'The Quote title',
+                quoteContent: 'The quote content'
+            };
+        },
         components: {
-            appUser: User
+            appQuote: Quote
         }
     }
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
 </style>
