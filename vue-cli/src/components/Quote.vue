e54810797a10442b2f834e10ee99d356ec850b8a
Daniel González <d.g.g69@me.com>
2017-02-17 20:59:26 -0600

    Finished section 9, Class 124

---
+++ vue-cli/src/components/Quote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div>
+            <slot name="title"></slot>
+            <span>
+                <slot name="subtitle">This a default subtitle</slot>
+            </span>
+        </div>
+        <hr>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss" scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px #4c4c4c;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+
+    h2 {
+        color: red;
+    }
+</style>
