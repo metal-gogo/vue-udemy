a193879264e1be68d3c0090ac9e0b540777f99cf
Daniel González <d.g.g69@me.com>
2017-02-19 22:25:10 -0600

    Finished Section 11, Class 152

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -95,6 +95,11 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -124,7 +129,7 @@
                         </ul>
                         <p>Gender: {{ userData.gender }} </p>
                         <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -133,7 +138,12 @@
 </template>
 
 <script>
+    import Switch from './components/Switch.vue';
+
     export default {
+        components: {
+            appSwitch: Switch
+        },
         data() {
             return {
                 userData: {
@@ -149,7 +159,8 @@
                     'Medium',
                     'Low'
                 ],
-                selectedPriority: 'High'
+                selectedPriority: 'High',
+                dataSwitch: true
             };
         }
     }
