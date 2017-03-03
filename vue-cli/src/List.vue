f72fa0c46208ee49454af614998a18bb83bbd0f0
Daniel González <d.g.g69@me.com>
2017-03-03 14:29:49 -0600

    Finished  S13. E174: Understanding mixins

---
+++ vue-cli/src/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Filters & Mixins</h1>
+    <hr>
+    <input v-model="filterText">
+    <ul v-for="fruit in filteredFruits">
+      <li>{{ fruit }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+        text: 'Hello there!',
+      };
+    },
+    computed: {
+      filteredFruits() {
+        return this.fruits.filter((element) => {
+          return element.match(this.filterText);
+        });
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
