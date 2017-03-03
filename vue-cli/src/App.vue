736a8164b537a79399e456d98eea5331554932b0
Daniel González <d.g.g69@me.com>
2017-03-03 14:02:29 -0600

    Finished S13 E170: Local filters

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,62 +1,27 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Built-in Directives</h1>
-                <p v-text="'Some text'"></p>
-                <p v-html="'<strong>Some html</strong>'"></p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Custom Directives</h1>
-                <p v-highlight:background="'violet'">Color this background</p>
-                <p v-highlight:background.delayed="'violet'">Color this background</p>
-                <p v-local-highlight:background.delayed.blink="{mainColor: 'violet', secondColor: 'turquoise', blinkTime: 500}">Color this background</p>
-                <p v-highlight:color="'green'">Color this text</p>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUpperCase}}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        directives: {
-            'local-highlight': {
-                bind(el, binding, vnode) {
-                    let delay = 0;
-                    if ( binding.modifiers['delayed'] ) {
-                        delay = 3000;
-                    }
-                    if ( binding.modifiers['blink'] ) {
-                        let mainColor = binding.value.mainColor;
-                        let secondColor = binding.value.secondColor;
-                        let currentColor = mainColor;
-                        setTimeout(() => {
-                            setInterval(() => {
-                                currentColor === secondColor ?
-                                    currentColor = mainColor :
-                                    currentColor = secondColor;
-                                if( binding.arg === 'background' ) {
-                                    el.style.backgroundColor = currentColor;
-                                } else {
-                                    el.style.color = currentColor;
-                                }
-                            }, binding.value.blinkTime);
-                        }, delay);
-                    } else {
-                        setTimeout(() => {
-                            if( binding.arg === 'background' ) {
-                                el.style.backgroundColor = binding.value.mainColor;
-                            } else {
-                                el.style.color = binding.value.mainColor;
-                            }
-                        }, delay);
-                    }
-                }
-            }
-        }
+  export default {
+    data() {
+      return {
+        text: 'Hello there!',
+      };
+    },
+    filters: {
+      toUpperCase(value) {
+        return value.toUpperCase();
+      },
     }
+  }
 </script>
 
 <style>
