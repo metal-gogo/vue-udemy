6181083c18025f37a5abd1eca7f8720def5205b4
Daniel González <d.g.g69@me.com>
2017-03-06 16:08:34 -0600

    Finished S17, E265: Two way binding

--- Section17/src/App.vue
+++ Section17/src/App.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Vuex</h1>
-                <app-result></app-result>
-                <hr>
-                <app-another-result></app-another-result>
-                <hr>
-                <app-counter ></app-counter>
-                <hr>
-                <app-another-counter ></app-another-counter>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vuex</h1>
+        <app-result></app-result>
+        <hr>
+        <app-another-result></app-another-result>
+        <hr>
+        <app-counter ></app-counter>
+        <hr>
+        <app-another-counter ></app-another-counter>
+        <hr>
+        <input type="text" v-model="value">
+        <p>{{ value }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -27,7 +30,22 @@
             appAnotherCounter: AnotherCounter,
             appResult: Result,
             appAnotherResult: AnotherResult,
-        }
+        },
+        methods: {
+          updateValue(event) {
+            this.$store.dispatch('updateValue', event.target.value);
+          },
+        },
+        computed: {
+          value: {
+            get() {
+              return this.$store.getters.value;
+            },
+            set(value) {
+              this.$store.dispatch('updateValue', value);
+            }
+          },
+        },
     }
 </script>
 
